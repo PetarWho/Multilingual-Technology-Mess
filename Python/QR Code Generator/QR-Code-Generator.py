617655563743e240302c7bb2ac604a4accfca162
Roy <92612927+PetarWho@users.noreply.github.com>
2022-10-20 14:21:31 +0300

    Updated QR-Code Generator

--- Python/QR Code Generator/QR-Code-Generator.py
+++ Python/QR Code Generator/QR-Code-Generator.py
@@ -3,13 +3,17 @@ import png
 from pyqrcode import QRCode
 
 # String which represents the QR Code
-s = "https://bit.ly/3LP9wm6"
+test = "https://bit.ly/3LP9wm6"
+
+s = input()
+
+fileName = input('File name: ')
 
 # Generate QR Code
 url = pyqrcode.create(s)
 
 # Create and save the SVG file and giving it a name
-url.svg("sickQR.svg", scale=8)
+url.svg(f"{fileName}.svg", scale=8)
 
 # Create and save the PNG file and giving it a name
-url.png("sickQR.png", scale=6)
+url.png(f"{fileName}.png", scale=6)
